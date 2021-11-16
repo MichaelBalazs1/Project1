b7c4c18ccb8bc5ba8539719de1fb46bbfa00b1f0
MichaelBalazs1 <94419134+MichaelBalazs1@users.noreply.github.com>
2021-11-15 18:01:59 -0800

    Add files via upload

---
+++ build/CMakeFiles/Pr1Plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pr1Plugin.dir/script/Pr1Plugin.cpp.o"
+  "libPr1Plugin.pdb"
+  "libPr1Plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pr1Plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ script/Pr1Plugin.cpp
@@ -0,0 +1,20 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+	{
+	class WorldPluginProject1 : public WorldPlugin
+		{
+		public: WorldPluginProject1() : WorldPlugin()
+			{
+			printf("Welcome to Michael's World!\n");
+			}
+		public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+			{
+			}
+
+		};
+GZ_REGISTER_WORLD_PLUGIN(WorldPluginProject1)
+
+	}
+
+
